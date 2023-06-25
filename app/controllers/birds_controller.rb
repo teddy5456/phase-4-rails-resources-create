9c6eb15e797db60d27f418a04bf7fc566fd39329
teddy <teddywambua5456@gmail.com>
2023-06-26 00:28:43 +0300

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,7 +5,11 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds
   end
-
+  
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
